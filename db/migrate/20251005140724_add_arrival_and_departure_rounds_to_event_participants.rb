d7fdcc17d7fc92ff02fe46bb7837d0b01acd5ca9
TAKUYA382 <dream.taku1102@gmail.com>
2025-10-06 17:06:44 +0900

    Remove duplicate migration file for arrival/leave rounds

--- db/migrate/20251005140724_add_arrival_and_departure_rounds_to_event_participants.rb
+++
@@ -1,6 +0,0 @@
-class AddArrivalAndDepartureRoundsToEventParticipants < ActiveRecord::Migration[7.2]
-  def change
-    add_column :event_participants, :arrival_round, :integer
-    add_column :event_participants, :departure_round, :integer
-  end
-end
